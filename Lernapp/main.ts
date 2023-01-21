0d7dc6ff99e871ab05c755d9e0757d852b3e2fed
LauraKupferschmid <116755408+LauraKupferschmid@users.noreply.github.com>
2023-01-21 19:13:06 +0100

    Update main.ts

--- Lernapp/main.ts
+++ Lernapp/main.ts
@@ -1,94 +1,141 @@
 namespace Endaufgabe {
 
-/*FRagen HTML*/
-    let frage1h = "Frage 1"
-    let frage2h = "Frage 2"
-    let frage3h = "Frage 3"
-    let frage4h = "Frage 4"
-    let frage5h = "Frage 5"
+    // Madeleine Hansen hat mich auf die Idee mit dem Interface gebracht
+    //Interface für Fragen erstellen
 
-/*Antworten Frage 1-5 HTML*/
-    let antwort1t = "Antwor 1 Richtig"
-    let antwort1f1 = "Antwor 1 false"
-    let antwort1f2 = "Antwor 1 false"
-    
-    let antwortenf2 = [antwort1t,antwort1f1,antwort1f2]
+    interface Frage {
+        question: string[];
+        antworten_richtig: string[];
+        anwser_true: boolean[];
+        anwser_false1: string[];
+        anwser_false2: string[];
+    }
 
-    let antwort2t = "Antwor 2 true"
-    let antwort2f1 = "Antwor 2 false"
-    let antwort2f2 = "Antwor 2 False"
+    //Fragen für HTML
+    let HTML_Fragen: Frage[] = [
+        {
+            question: ["wer ist besser htm", "FRage2html", "FRage3html", "Frage4 ht", "Frage5html"],
+            antworten_richtig: ["antwort1", "antwort2", "antwort3", "antwort4", "antwort5"],
+            anwser_true: [true, true, true, true, true],
+            anwser_false1: ["alphabet", "falscheant2", "falscheant3", "falscheant4", "falscheant5"],
+            anwser_false2: ["noch mehr falsche antworten", "eine weitere falsche anwort", "falsche ant3", "4te falsche an", "falsche die 5te"]
+        },
+    ]
 
-    let antwortenf1 = [antwort2t,antwort2f1,antwort1f2]
+    //Kontrolle von HTML_Fragen
+    console.log(HTML_Fragen[0].antworten_richtig[1]);
 
-    let antwort3t = "Antwor 3 true"
-    let antwort3f1 = "Antwor 3 false"
-    let antwort3f2 = "Antwor 3 ture"
-    
-    let antwortenf3 = [antwort3t,antwort3f1,antwort3f2]
+    //Fragen für CSS
+    let CSS_Fragen: Frage[] = [
+        {
+            question: ["wer ist besser css", "FRage2 css", "FRage3 css", "Frage4 css", "Frage5 css"],
+            antworten_richtig: ["antwort1", "antwort2 css", "antwort3", "antwort4", "antwort5"],
+            anwser_true: [true, true, true, true, true],
+            anwser_false1: ["alphabet", "falscheant2 css lol", "falscheant3", "falscheant4", "falscheant5"],
+            anwser_false2: ["noch mehr falsche antworten", "eine weitere falsche anwort css again lol", "falsche ant3", "4te falsche an", "falsche die 5te"]
+        },
+    ]
 
-    let antwort4t = "Antwor 4 true"
-    let antwort4f1 = "Antwor 4 false"
-    let antwort4f2 = "Antwor 4 false"
+    //Kontrolle von CSS_Fragen
+    console.log(CSS_Fragen[0].anwser_false1[2]);
 
-    let antwortenf4 = [antwort4t,antwort4f1,antwort4f2]
-    
-    let antwort5t = "Antwor 5 true"
-    let antwort5f1 = "Antwor 5 false"
-    let antwort5f2 = "Antwor 5 false"
+    //Fragen für Typescript
+    let TS_Fragen: Frage[] = [
+        {
+            question: ["wer ist besser", "FRagets2", "FRage3", "Frage4", "Frage5"],
+            antworten_richtig: ["antwort1", "antwortts2", "antwort3", "antwort4", "antwort5"],
+            anwser_true: [true, true, true, true, true],
+            anwser_false1: ["alphabet", "falscheantts2", "falscheant3", "falscheant4", "falscheant5"],
+            anwser_false2: ["noch mehr falsche antworten", "eine weitere ts falsche anwort", "falsche ant3", "4te falsche an", "falsche die 5te"]
+        },
+    ]
 
-    let antwortenf5 = [antwort5t,antwort5f1,antwort5f2]
+    //Div boxendeklarieren um classlist.add und remuve zu machen
+    let katbox = document.querySelector("#kategoriebox")
+    let frabox = document.querySelector("#frageseite")
 
-    let frageh =[frage1h,frage2h,frage3h,frage4h,frage5h];
+    //Kontrolle von TS_Fragen
+    console.log(TS_Fragen[0].anwser_false2[3]);
 
-    /*Element für Fragen*/
+    //Headline Element für Fragen erstellen mit TS//
+
     let frage: HTMLElement = document.createElement("headline1")
-    frage.innerHTML= frage1h
+    frage.classList.add(".frage")
     document.querySelector(".Frage").appendChild(frage);
 
-    /*Buttons für Antworten*/
-    let myButton1: HTMLElement = document.createElement("button")
-    myButton1.innerHTML= antwort1t
-    document.querySelector(".Antworten").appendChild(myButton1);
+    /*Buttons für Antworten erstellen mit TS*/
+    let antwort1: HTMLButtonElement = document.createElement("button")
+    antwort1.classList.add('antwortfeld1')
+    document.querySelector(".Antworten").appendChild(antwort1);
 
-    let antwort2: HTMLElement = document.createElement("button")
-    antwort2.innerHTML= antwort1f1
+    let antwort2: HTMLButtonElement = document.createElement("button")
+    antwort2.classList.add('antwortfeld2')
     document.querySelector(".Antworten").appendChild(antwort2);
 
-    let antwort3: HTMLElement = document.createElement("button")
-    antwort3.innerHTML= antwort1f2
+    let antwort3: HTMLButtonElement = document.createElement("button")
+    antwort3.classList.add('antwortfeld3')
     document.querySelector(".Antworten").appendChild(antwort3)
 
-    
+    //auf den klick auf eine kategorie soll die klasse zugewiesen und entfernt werden.
+    //document.querySelector("#htmlbtn").addEventListener('click',function(){})
+    //html button wird geklickt also sollen die fragen aus HTML_Fragen geladen werden. 
+    //dabei soll der index math.random sein damit die fragen zufällig ausgeschmissen werden.
+    //if html button geklickt wird soll das passieren
+    // if css button... und so weiter
+
+    let index = Math.floor(Math.random()*4)+1;
+
+    document.querySelector('#htmlbtn').addEventListener('click', function () {
+        //klasse entfernen und zuweisen
+        katbox.classList.add('hidden');
+        frabox.classList.remove('hidden');
+        // innerhtml zuweisen
+        frage.innerHTML = HTML_Fragen[0].question[index];
+        antwort1.innerHTML = HTML_Fragen[0].antworten_richtig[index];
+        antwort2.innerHTML = HTML_Fragen[0].anwser_false1[index];
+        antwort3.innerHTML = HTML_Fragen[0].anwser_false2[index]
+    })
 
 
+    document.querySelector("#cssbtn").addEventListener('click', function () {
+        katbox.classList.add('hidden');
+        frabox.classList.remove('hidden');
+        frage.innerHTML = CSS_Fragen[0].question[index];
+        antwort1.innerHTML = CSS_Fragen[0].antworten_richtig[index];
+        antwort2.innerHTML = CSS_Fragen[0].anwser_false1[index];
+        antwort3.innerHTML = CSS_Fragen[0].anwser_false2[index]
+    })
+
+    document.querySelector("#tsbtn").addEventListener('click', function () {
+        katbox.classList.add('hidden');
+        frabox.classList.remove('hidden');
+        frage.innerHTML = TS_Fragen[0].question[index];
+        antwort1.innerHTML = TS_Fragen[0].antworten_richtig[index];
+        antwort2.innerHTML = TS_Fragen[0].anwser_false1[index];
+        antwort3.innerHTML = TS_Fragen[0].anwser_false2[index];
+    })
+
+    document.querySelector("#gmbtn").addEventListener('click', function () {
+        katbox.classList.add('hidden'),
+            frabox.classList.remove('hidden')
+        function setall() {
+            console.log("test hallo :)")
+        }
+        setall
+    })
+
+    //let i = Math.floor(Math.random()*6)+1;
+
+    //document.querySelector("#mix").addEventListener('click',function(){
+    // audioliste = [];
+    // while(audioliste.length < 3) {
+    //let i = Math.floor(Math.random()*8)+1;
+    //audioliste.push(audiomix[i]);
+    //}
+
+    // function dosomething{ (
+    // if (antwort1.getElement ddEventListener('click', myButton1)
+    // window.alert) 
 
 
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 }
